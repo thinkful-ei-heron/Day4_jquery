cd041d1c3f28ddb7edf1c2cf7c73064f1cf62b20
julio-crypt0 <hernandezjulio58@ymail.com>
2019-09-19 11:30:07 -0500

    added explanations

--- partnerdrills.js
+++ partnerdrills.js
@@ -1,9 +1,11 @@
 'use strict';
 function changeCatPic() {
   $('.thumbnail').on('click', function(event) {
-    const imgSrc = $(this).find('img').attr('src');  
-    const imgAlt = $(this).find('img').attr('alt');         //Spent 30 mins on this one, couldn't solve. also
-    $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);  //meant to be accompanied with html and css files from replt.it
-  });                                                           // url to repl.it w/ sol : https://repl.it/@juli0crypto/Cat-carousel-jQuery-1
-}
+    const imgSrc = $(this).find('img').attr('src');  //this line sets a variable on the image the is clicked linking the src attribute
+    const imgAlt = $(this).find('img').attr('alt');  //this line does the same as above but instead copies the alt attribute
+    $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);  //this line searches for the ('.hero img') class / attr and sets the old
+  });                                                           
+}                                                            //src : 'src' to the new one that we linked earlier and also the alt attr.
 $(changeCatPic);
+
+// url to repl.it w/ sol : https://repl.it/@juli0crypto/Cat-carousel-jQuery-1
